aeea8813e2265deb5ea5c6a29ed1b96777dfb564
JakobBundgaard <56150649+JakobBundgaard@users.noreply.github.com>
2022-09-16 12:04:54 +0200

    Core framework done

--- BT_PHP_MVC/bundgaardmvc/app/views/pages/index.php
+++ BT_PHP_MVC/bundgaardmvc/app/views/pages/index.php
@@ -4,11 +4,7 @@ require APP_ROOT . '/views/includes/header.php';
 
 
     <h1><?php echo $data['title']; ?></h1>
-    <ul>
-        <?php foreach($data['posts'] as $post) : ?>
-             <li><?php echo $post->title; ?></li>
-        <?php endforeach; ?>
-    </ul>
+    
 
 <?php 
 require APP_ROOT . '/views/includes/footer.php';
