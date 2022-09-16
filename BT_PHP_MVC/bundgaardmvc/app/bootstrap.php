b88b83c68cb7dc01e8a5984ecf3b523e0171ab22
JakobBundgaard <56150649+JakobBundgaard@users.noreply.github.com>
2022-09-16 09:10:26 +0200

    Aoutoload for core libraries added

--- BT_PHP_MVC/bundgaardmvc/app/bootstrap.php
+++ BT_PHP_MVC/bundgaardmvc/app/bootstrap.php
@@ -2,11 +2,12 @@
 // The bootstrap file is where we require all our files, 
 // instead of requiring them in the index file.
 
-// Loading libraries
-require_once 'libraries/core.php';
-require_once 'libraries/controller.php';
-require_once 'libraries/database.php';
+// Load config file
+require_once 'config/config.php';
 
-
+// Autoload core libraries so we dont have to manually require every file we need
+spl_autoload_register(function($className) {
+    require_once 'libraries/' . $className . '.php';
+});
 
 ?>
---
+++ BT_PHP_MVC/bundgaardmvc/app/config/config.php
@@ -0,0 +1,11 @@
+<?php 
+// Defining constants
+// App route
+define('APP_ROOT', dirname(dirname(__FILE__)));
+
+
+// Url route
+define('URL_ROOT', 'http://localhost/BT_PHP_MVC/bundgaardmvc');
+
+// Sitename
+define('SITE_NAME', 'BundgaardMVC');
